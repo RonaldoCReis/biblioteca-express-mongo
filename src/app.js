863995c8ae9ad61136bbfdaa5bc26d7adc559eb0
Ronaldo Reis <ronaldocreis7@gmail.com>
2023-11-07 22:01:46 -0300

    rota livros

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from 'express';
 
 const app = express();
 
+const livros = [
+  {
+    id: 1,
+    nome: 'Senhor dos Anéis',
+  },
+  {
+    id: 2,
+    nome: 'Harry Potter',
+  },
+];
+
 app.get('/', (req, res) => {
   res.status(200).send('Curso de Node.js');
+});
+
+app.get('/livros', (req, res) => {
+  res.status(200).json(livros);
 });
 
 export default app;
